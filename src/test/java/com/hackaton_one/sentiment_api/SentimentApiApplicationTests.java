0ae966f0d418d5c01bda6de220b15facda326ff4
Patricia <patriciastarck@gmail.com>
2025-12-12 13:04:59 -0300

    Subindo estrutura inicial do projeto Spring Boot

---
+++ src/test/java/com/hackaton_one/sentiment_api/SentimentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton_one.sentiment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
